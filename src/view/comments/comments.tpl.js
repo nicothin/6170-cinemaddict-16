5598058ab969759f6a58e376ac40001daee41c57
Nikolay Gromov <nicothin@gmail.com>
2021-12-20 00:07:14 +0300

    Убрал ненужный коммент

--- src/view/comments/comments.tpl.js
+++ src/view/comments/comments.tpl.js
@@ -3,10 +3,6 @@ import { EMOTIONS } from '../../constants';
 export const createComments = (comments) => {
   const commentsCounter = comments.length;
 
-  // const comments = movie.comments.map((comment) => new Comment(
-  //   generateComment(comment)
-  // ));
-
   const emotionsList = EMOTIONS.map((emotion) => `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emotion}" value="${emotion}">
   <label class="film-details__emoji-label" for="emoji-${emotion}">
     <img src="./images/emoji/${emotion}.png" width="30" height="30" alt="emoji">
